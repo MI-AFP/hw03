2dec76e719b6fa71281961e8d841eb775b6060fe
Marek Suchánek <marek.suchanek@gmail.com>
2018-02-06 18:46:42 +0100

    Tasks draft and tests init

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,97 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+
+-------------------------------------------------------------------------------
+-- DO NOT EDIT DATA TYPES!
+data MaritalStatus = Single | Married | Widowed
+                   deriving (Show, Read, Eq)
+
+data Gender = Male | Female
+            deriving (Show, Read, Eq)
+
+data AcademicTitle = DiS | Bc | Mgr | Ing | PhDr | MUDr | PhD | Doc | Prof
+                   deriving (Show, Read, Ord, Eq, Bounded, Enum)
+
+data Person = Person { pFirstname     :: String
+                     , pLastname      :: String
+                     , pGender        :: String
+                     , pMaritalStatus :: String
+                     , pATitles       :: [AcademicTitle]
+                     }
+
+-- | Full czech salutation (in nominative - i.e. první pád)
+-- |
+-- | "pan doktor Pavel Novák", "paní inženýrka Karolína Šťastná"
+-- | "slečna Petra Králová", "Jan" (kid)
+-- | if younger than 15 -> kid -> just firstname
+-- | if female younger than 25 without academic title and single -> "slečna"
+-- | otherwise "pan/paní" depending on the gender
+-- | if academic titles, pick the most important (nothing for DiS and Bc)
+-- |
+-- | https://www.muni.cz/o-univerzite/uredni-deska/oslovovani-akademickych-pracovniku
+-- | http://www.etiketavse.estranky.cz/clanky/etiketa/4.-oslovovani-a-spolecenska-vyznamnost.html
+-- TODO: implement czech salutation which passes the tests
+czechSalutation :: Person -> String
+czechSalutation = undefined
+
+-------------------------------------------------------------------------------
+-- DO NOT EDIT DATA TYPE!
+-- https://en.wikipedia.org/wiki/Allen%27s_interval_algebra
+--                                          X      Y
+data AllensIAlgebraRelation a = Equals   (a, a) (a, a) -- X = Y
+                              | Before   (a, a) (a, a) -- X < Y
+                              | Meets    (a, a) (a, a) -- X m Y
+                              | Overlaps (a, a) (a, a) -- X o Y
+                              | Starts   (a, a) (a, a) -- X s Y
+                              | During   (a, a) (a, a) -- X d Y
+                              | Finishes (a, a) (a, a) -- X f Y
+                             deriving (Show, Read, Eq)
+
+-- | Compare two intervals given as tuples and return appropriate
+-- | Allen's Interval Algebra relation between them
+-- TODO: implement Allen's algebra relation detection of intervals
+allensComparison :: Ord a => (a, a) -> (a, a) -> AllensIAlgebraRelation a
+allensComparison = undefined
+
+-------------------------------------------------------------------------------
+-- DO NOT EDIT DATA TYPE!
+data Shape2D = Circle { spRadius :: Int }
+             | Square { sqSide :: Int }
+             | Rectangle { reWidth :: Int, reHeight :: Int }
+             | Triangle { triSideA :: Int, triSideB :: Int, triSideC :: Int }
+             deriving (Show, Read, Eq)
+
+-- TODO: implement circumference calculation for 2D shapes
+shapeCircumference :: Shape2D -> Int
+shapeCircumference = undefined
+
+-- TODO: implement area calculation for 2D shapes
+shapeArea :: Shape2D -> Int
+shapeArea = undefined
+
+-------------------------------------------------------------------------------
+-- | Geometric series as infinite list
+-- | https://en.wikipedia.org/wiki/Geometric_series
+-- TODO: implement geometric series
+geometricSeries :: Integer -> Integer -> [Integer]
+geometricSeries a r = undefined
+
+-- TODO: implement infinite list of primes [2, 3, 5, 7, 11, ...]
+primes :: [Integer]
+primes = undefined
+
+-- | Euler's totient function
+-- | https://en.wikipedia.org/wiki/Euler%27s_totient_function
+-- TODO: implement phi(n) by using search in primes
+phi :: Integer -> Integer
+phi n = undefined
+
+-------------------------------------------------------------------------------
+-- !!! DO NOT COPY, JUST IMPORT (avoid conflicts, pick the best option for you)
+-- iii visit the content of modules
+-- TODO: replace undefined with "example1" from Data.DummyList.Examples module
+dummyListExample1 = undefined
+-- TODO: replace undefined with "example2" from Examples module
+stringExample2 = undefined
+-- TODO: replace undefined with "example2" from Examples module
+stringExample3 = undefined
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,90 @@
+import Test.Hspec
+
+import Lib
+
+-- `main` is here so that this module can be run from GHCi on its own.  It is
+-- not needed for automatic spec discovery.
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec spec
+
+spec :: Spec
+spec = do
+  describe "czechSalutation" $ do
+    undefined
+
+  describe "allensComparison" $ do
+    it "returns Equals for equal internals" $ do
+      allensComparison (1, 2) (1, 2) `shouldBe` Equals (1, 2) (1, 2)
+      allensComparison (-2, 2) (-2, 2) `shouldBe` Equals (-2, 2) (-2, 2)
+      allensComparison ('a', 'z') ('a', 'z') `shouldBe` Equals ('a', 'z') ('a', 'z')
+    it "returns Before when end of one is before start of other" $ do
+      allensComparison (1, 2) (4, 5) `shouldBe` Before (1, 2) (4, 5)
+      allensComparison (-2, 0) (1, 2) `shouldBe` Equals (-2, 0) (1, 2)
+      allensComparison ('a', 'c') ('x', 'z') `shouldBe` Equals ('a', 'c') ('x', 'z')
+    it "returns Before when end of one is before start of other (inversed)" $ do
+      allensComparison (4, 5) (1, 2) `shouldBe` Before (1, 2) (4, 5)
+      allensComparison (1, 2) (-2, 0) `shouldBe` Before (-2, 0) (1, 2)
+      allensComparison ('x', 'z') ('a', 'c') `shouldBe` Before ('a', 'c') ('x', 'z')
+    it "returns Meets when end of one is equal to start of other" $ do
+      allensComparison (1, 2) (2, 5) `shouldBe` Meets (1, 2) (2, 5)
+      allensComparison (-2, 0) (0, 2) `shouldBe` Meets (-2, 0) (0, 2)
+      allensComparison ('a', 'c') ('c', 'z') `shouldBe` Meets ('a', 'c') ('c', 'z')
+    it "returns Meets when end of one is equal to start of other (inversed)" $ do
+      allensComparison (2, 5) (1, 2) `shouldBe` Meets (1, 2) (2, 5)
+      allensComparison (0, 2) (-2, 0) `shouldBe` Meets (-2, 0) (0, 2)
+      allensComparison ('c', 'z') ('a', 'c') `shouldBe` Meets ('a', 'c') ('c', 'z')
+    it "returns Overlaps when there is common part but first starts sooner and second end later" $ do
+      allensComparison (1, 4) (2, 5) `shouldBe` Meets (1, 4) (2, 5)
+      allensComparison (-2, 1) (-1, 2) `shouldBe` Meets (-2, 1) (-1, 2)
+      allensComparison ('a', 'm') ('i', 'z') `shouldBe` Meets ('a', 'm') ('i', 'z')
+    it "returns Overlaps when there is common part but first starts sooner and second end later (inversed)" $ do
+      allensComparison (2, 5) (1, 4) `shouldBe` Meets (1, 4) (2, 5)
+      allensComparison (-1, 2) (-2, 1) `shouldBe` Meets (-2, 1) (-1, 2)
+      allensComparison ('i', 'z') ('a', 'm') `shouldBe` Meets ('a', 'm') ('i', 'z')
+    it "returns Starts if one starts with the other but ends sooner" $ do
+      allensComparison (1, 5) (1, 10) `shouldBe` Starts (1, 5) (1, 10)
+      allensComparison (-1, 2) (-1, 7) `shouldBe` Meets (-1, 2) (-1, 7)
+      allensComparison ('b', 'e') ('b', 'm') `shouldBe` Meets ('b', 'e') ('b', 'm')
+    it "returns Starts if one starts with the other but ends sooner (inversed)" $ do
+      allensComparison (1, 10) (1, 5) `shouldBe` Starts (1, 5) (1, 10)
+      allensComparison (-1, 7) (-1, 2) `shouldBe` Meets (-1, 2) (-1, 7)
+      allensComparison ('b', 'm') ('b', 'e') `shouldBe` Meets ('b', 'e') ('b', 'm')
+    it "returns Finishes if one ends with the other but starts sooner" $ do
+      allensComparison (5, 10) (1, 10) `shouldBe` Starts (5, 10) (1, 10)
+      allensComparison (2, 7) (-1, 7) `shouldBe` Meets (2, 7) (-1, 7)
+      allensComparison ('e', 'm') ('b', 'm') `shouldBe` Meets ('e', 'm') ('b', 'm')
+    it "returns Finishes if one ends with the other but starts sooner (inversed)" $ do
+      allensComparison (1, 10) (5, 10) `shouldBe` Starts (5, 10) (1, 10)
+      allensComparison (-1, 7) (2, 7) `shouldBe` Meets (2, 7) (-1, 7)
+      allensComparison ('b', 'm') ('e', 'm') `shouldBe` Meets ('e', 'm') ('b', 'm')
+    it "returns During if one starts after the other and also ends before the other" $ do
+      allensComparison (5, 10) (1, 20) `shouldBe` Starts (5, 10) (1, 20)
+      allensComparison (2, 7) (-1, 10) `shouldBe` Meets (2, 7) (-1, 10)
+      allensComparison ('e', 'm') ('a', 'z') `shouldBe` Meets ('e', 'm') ('a', 'z')
+    it "returns During if one starts after the other and also ends before the other (inversed)" $ do
+      allensComparison (1, 20) (5, 10) `shouldBe` Starts (5, 10) (1, 20)
+      allensComparison (-1, 10) (2, 7) `shouldBe` Meets (2, 7) (-1, 10)
+      allensComparison ('a', 'z') ('e', 'm') `shouldBe` Meets ('e', 'm') ('a', 'z')
+
+  describe "shapeCircumference" $ do
+    undefined
+
+  describe "shapeArea" $ do
+    undefined
+
+  describe "geometricSeries" $ do
+    undefined
+
+  describe "primes" $ do
+    undefined
+
+  describe "phi" $ do
+    undefined
+
+  describe "imports" $ do
+    it "has correct example 1"
+      take 100 dummyListExample1 `shouldBe` replicate 100 25
+    it "has correct example 2"
+      stringExample2 `shouldBe` "Example 2"
+    it "has correct example 3"
+      stringExample2 `shouldBe` "Example 3"
